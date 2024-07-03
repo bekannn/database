f2f838f6dfb6b000e69c9a75ce72eced688300fc
Bhornpatr <bhornpatr@gmail.com>
2024-07-03 20:52:28 +0700

    Initial commit with database schema and tables

--- database.sql
+++ database.sql
@@ -1,5 +1,97 @@
-CREATE TABLE users (
-    id INT PRIMARY KEY,
+CREATE TABLE Customers (
+    customer_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(10) NOT NULL,
+    address VARCHAR(100) NOT NULL
 );
+--CREATE TYPE user_role AS ENUM ('Admin', 'Investor');
+
+
+CREATE TYPE product_choice AS ENUM ('Sleeveless', 'T-shirt', 'Long-sleeve', 'Trouser', 'Shorts', 'Skirt');
+CREATE TABLE Products (
+    product_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    category product_choice NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    quantity INT NOT NULL
+);
+
+CREATE TYPE investment_type_choice AS ENUM (
+    'Equity Investment',
+    'Debt Investment',
+    'Convertible Notes',
+    'Crowdfunding',
+    'Strategic Partnership',
+    'Impact Investment',
+    'Revenue Sharing',
+    'Other'
+);
+--create ENUM type to specify the investment type
+
+CREATE TABLE Investors (
+    investor_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone VARCHAR(10) NOT NULL,
+    investment_type investment_type_choice NOT NULL,
+    investment_amount DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE Investments (
+    investment_id SERIAL PRIMARY KEY,
+    investor_id INT REFERENCES Investors(investor_id),
+    investment_type investment_type_choice NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    investment_date DATE NOT NULL
+);
+
+CREATE TABLE Financial_Metrics (
+    metrc_id SERIAL PRIMARY Key,
+    metric_name VARCHAR(50) NOT NULL,
+    value DECIMAL(10, 2) NOT NULL,
+    metric_date DATE NOT NULL
+);
+
+
+
+/*CREATE TABLE Users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone INT(10) NOT NULL,
+    address VARCHAR(100) NOT NULL
+);
+CREATE TYPE user_role AS ENUM ('Admin', 'Investor');
+
+
+CREATE TABLE Portfolios (
+    portfoli_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES Users(user_id),
+    portfoli_name VARCHAR(100) NOT NULL
+);
+
+CREATE TYPE investment_type_choice AS ENUM ('Stock', 'Bond', 'ETF', 'Mutual Fund', 'Crypto');
+--create ENUM type to specify the investment type
+
+CREATE TABLE Investments (
+    investment_id SERIAL PRIMARY KEY,
+    portfoli_id INT REFERENCES Portfolios(portfoli_id),
+    ticker VARCHAR(10) NOT NULL,
+    investment_type investment_type_choice NOT NULL,
+    purchase_date DATE NOT NULL,
+    purchase_price DECIMAL(10, 2) NOT NULL,
+    quantity INT NOT NULL,
+    current_price DECIMAL(10, 2),
+)
+
+CREATE TYPE transaction_choice AS ENUM ('Buy', 'Sell');
+
+CREATE TABLE Transactions (
+    transaction_id SERIAL PRIMARY Key,
+    investment_id INT REFERENCES Investments(investment_id),
+    transaction_date DATE NOT NULL,
+    transaction_type transaction_choice NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    quantity INT NOT NULL
+); */
